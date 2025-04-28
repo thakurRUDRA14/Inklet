cb66243049407d8da6d6018777f70559faa0c7f7
Rudra Pratap Singh <153213328+thakurRUDRA14@users.noreply.github.com>
2025-04-29 03:14:58 +0530

    Initiliaze Database(Prisma)

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,8 +1,36 @@
 import { Hono } from 'hono'
+import { PrismaClient } from '@prisma/client/edge'
+import { withAccelerate } from '@prisma/extension-accelerate'
 
-const app = new Hono()
 
-app.get('/', (c) => {
+const app = new Hono<{
+  Bindings:{
+    DATABASE_URL: string
+  }
+}>()
+
+app.post('/api/v1/signup', (c) => {
+  const prisma = new PrismaClient(
+    {
+      datasourceUrl: c.env.DATABASE_URL
+    }
+  ).$extends(withAccelerate())
+  return c.text('Hello Hono!')
+})
+
+app.post('/api/v1/sighin', (c) => {
+  return c.text('Hello Hono!')
+})
+
+app.post('/api/v1/blog', (c) => {
+  return c.text('Hello Hono!')
+})
+
+app.put('/api/v1/blog', (c) => {
+  return c.text('Hello Hono!')
+})
+
+app.get('/api/v1/blog/:id', (c) => {
   return c.text('Hello Hono!')
 })
 
