17538163c9ffa82fa6e339c6b3c1674b45b39b7e
Rudra Pratap Singh <153213328+thakurRUDRA14@users.noreply.github.com>
2025-05-03 21:31:21 +0530

    Added user routes

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,37 +1,84 @@
-import { Hono } from 'hono'
-import { PrismaClient } from '@prisma/client/edge'
-import { withAccelerate } from '@prisma/extension-accelerate'
+import { Hono } from "hono";
+import { PrismaClient } from "@prisma/client/edge";
+import { withAccelerate } from "@prisma/extension-accelerate";
+import { sign } from "hono/jwt";
 
+type Bindings = {
+    DATABASE_URL: string;
+    JWT_SECRET: string;
+};
 
-const app = new Hono<{
-  Bindings:{
-    DATABASE_URL: string
-  }
-}>()
+const app = new Hono<{ Bindings: Bindings }>();
 
-app.post('/api/v1/signup', (c) => {
-  const prisma = new PrismaClient(
-    {
-      datasourceUrl: c.env.DATABASE_URL
+app.post("/api/v1/signup", async (c) => {
+    const prisma = new PrismaClient({
+        datasourceUrl: c.env.DATABASE_URL,
+    }).$extends(withAccelerate());
+
+    const body = await c.req.json();
+
+    try {
+        const user = await prisma.user.create({
+            data: {
+                name: body.name,
+                username: body.username,
+                password: body.password,
+            },
+        });
+
+        const jwt = await sign({ id: user.id }, c.env.JWT_SECRET);
+
+        return c.json({ jwt });
+    } catch (error) {
+        console.log(error);
+        return c.json({ error: "User already exists" }, 409);
     }
-  ).$extends(withAccelerate())
-  return c.text('Hello Hono!')
-})
+});
 
-app.post('/api/v1/sighin', (c) => {
-  return c.text('Hello Hono!')
-})
+app.post("/api/v1/signin", async (c) => {
+    const prisma = new PrismaClient({
+        datasourceUrl: c.env.DATABASE_URL,
+    }).$extends(withAccelerate());
 
-app.post('/api/v1/blog', (c) => {
-  return c.text('Hello Hono!')
-})
+    const body = await c.req.json();
 
-app.put('/api/v1/blog', (c) => {
-  return c.text('Hello Hono!')
-})
+    try {
+      
+        const user = await prisma.user.findFirst({
+            where: {
+                username: body.username,
+                password: body.password,
+            },
+        });
 
-app.get('/api/v1/blog/:id', (c) => {
-  return c.text('Hello Hono!')
-})
+        if (!user) {
+            return c.json({ error: "Incorrect Credentials" }, 403);
+        }
+        const jwt = await sign({ id: user.id }, c.env.JWT_SECRET);
+        return c.json({ jwt });
+    } 
+    
+    catch (error) {
+        console.log(error);
+        c.status(500);
+        return c.json({ error: "Internal Server Error" }, 500);
+    }
+});
 
-export default app
+app.post("/api/v1/blog", (c) => {
+    return c.text("Hello Hono!");
+});
+
+app.put("/api/v1/blog", (c) => {
+    return c.text("Hello Hono!");
+});
+
+app.get("/api/v1/blog/:id", (c) => {
+    return c.text("Hello Hono!");
+});
+
+app.get("/api/v1/blog/bulk", (c) => {
+    return c.text("Hello Hono!");
+});
+
+export default app;
