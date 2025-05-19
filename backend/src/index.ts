92ee18380fa694e7ddbe9828d3cda9417824418b
Rudra Pratap Singh <153213328+thakurRUDRA14@users.noreply.github.com>
2025-05-19 22:42:38 +0530

    feat: enable CORS for all routes in the application

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,6 +1,7 @@
 import { Hono } from "hono";
 import { userRouter } from "./routes/user";
 import { blogRouter } from "./routes/blog";
+import {cors} from "hono/cors"
 
 type Bindings = {
     DATABASE_URL: string;
@@ -8,7 +9,7 @@ type Bindings = {
 };
 
 const app = new Hono<{ Bindings: Bindings }>();
-
+app.use('/*',cors())
 app.route("/api/v1/user", userRouter);
 app.route("/api/v1/blog", blogRouter);
 
